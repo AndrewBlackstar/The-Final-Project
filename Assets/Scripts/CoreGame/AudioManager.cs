be3e1755696afc49ab9198cefc3df2b3015a815d
AndrewBlackstar <dracokaisar@gmail.com>
2025-03-28 10:59:22 -0500

    audio manager

--- Assets/Scripts/CoreGame/AudioManager.cs
+++ Assets/Scripts/CoreGame/AudioManager.cs
@@ -1,16 +1,69 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static AudioManager Instance { get; private set; }
+    public Sounds[] musicSound, SfxSound;
+    public AudioSource musicSource, sfxSource;
+
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        float volume = PlayerPrefs.GetFloat("Volume", 1f); // Si no hay valor guardado, usa 1
+        musicSource.volume = volume;
+        PlayMusic("BGMusic");
     }
+
+    public void PlayMusic(String name)
+    {
+        Sounds s = Array.Find(musicSound, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("musica no encontrado");
+        }
+
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.mute = false;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySfx(string name)
+    {
+        Sounds s = Array.Find(SfxSound, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("sfx no encontrado");
+        }
+
+        else
+        {
+            sfxSource.clip = s.clip;
+            musicSource.mute = false;
+            sfxSource.Play();
+        }
+    }
+
+
 }
+
+
+
+
+
+
--- Assets/Scripts/CoreUI/UIControl.cs
+++ Assets/Scripts/CoreUI/UIControl.cs
@@ -1,16 +1,118 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System;
 
 public class UIControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool isPaused = false;
+    public GameObject pausePanel;
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] Toggle muteToggle;
+
     void Start()
     {
+        pausePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (PlayerPrefs.HasKey("Muted"))
+        {
+            muteToggle.isOn = PlayerPrefs.GetInt("Muted") == 1;
+            SetMute();
+        }
+
+        if (PlayerPrefs.HasKey("musicVolume") && PlayerPrefs.HasKey("sfxVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSfxVolume();
+        }
+
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isPaused)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void LoadScene(string name)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(name);
+    }
+
+    public void SetMusicVolume()
+    {
+        float musicVolume = musicSlider.value;
+        audioMixer.SetFloat("Music", MathF.Log10(musicVolume)*20);
+        PlayerPrefs.SetFloat("Music", musicVolume);
         
+    }
+
+    public void SetSfxVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+        audioMixer.SetFloat("sfx", MathF.Log10(sfxVolume)*20);
+    }
+
+    public void SetMute()
+    {
+        if (muteToggle.isOn)
+        {
+            audioMixer.SetFloat("Music", -80f);
+            audioMixer.SetFloat("sfx", -80f);
+            PlayerPrefs.SetInt("Muted", 1); // Guardamos el estado de mute
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSfxVolume();
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetMusicVolume();
+        SetSfxVolume();
     }
 }
