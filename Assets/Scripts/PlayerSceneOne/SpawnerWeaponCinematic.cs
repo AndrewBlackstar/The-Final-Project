83559a315dbd5ba018393a45e9d5ea88ff20758d
AndrewBlackstar <dracokaisar@gmail.com>
2025-04-04 12:20:11 -0500

    bullet

--- Assets/Scripts/PlayerSceneOne/SpawnerWeaponCinematic.cs
+++ Assets/Scripts/PlayerSceneOne/SpawnerWeaponCinematic.cs
@@ -6,37 +6,41 @@ public class SpawnerWeaponCinematic : MonoBehaviour
 {
     public GameObject[] WeaponPrefab;
     public Transform spawnpont;
-    GameObject currentWeapon;
     public ParticleSystem spawnVfx;
 
-    private void Start()
-    {
-        currentWeapon.transform.Rotate(0, 0, 2);
-    }
+    private Coroutine currentSequence;
+    private GameObject currentWeapon;
+    private bool cancelRequested = false;
+    private bool hasBeenSkipped = false;
 
-    public void ShowWeaponSequence( )
+    public void ShowWeaponSequence()
     {
-        StartCoroutine(SpawnSequence(2));
+        if (hasBeenSkipped) return; // Evita que se vuelva a ejecutar tras el skip
+
+        cancelRequested = false;
+
+        if (currentSequence != null)
+            StopCoroutine(currentSequence);
+
+        currentSequence = StartCoroutine(SpawnSequence(2f));
     }
 
     IEnumerator SpawnSequence(float time)
     {
         foreach (var weapon in WeaponPrefab)
         {
-            if (currentWeapon != null)
-            {
-                Destroy(currentWeapon);
-            }
+            if (cancelRequested || hasBeenSkipped) yield break;
 
-            // Instancia el arma
+            if (currentWeapon != null)
+                Destroy(currentWeapon);
+
             currentWeapon = Instantiate(weapon, spawnpont.position, Quaternion.identity);
 
-            // Reproduce el VFX
             if (spawnVfx != null)
             {
                 ParticleSystem vfx = Instantiate(spawnVfx, spawnpont.position, Quaternion.identity);
                 vfx.Play();
-                Destroy(vfx.gameObject, vfx.main.duration + vfx.main.startLifetime.constantMax); // Limpieza automática
+                Destroy(vfx.gameObject, vfx.main.duration + vfx.main.startLifetime.constantMax);
             }
 
             yield return new WaitForSeconds(time);
@@ -45,12 +49,26 @@ public class SpawnerWeaponCinematic : MonoBehaviour
         HideWeapon();
     }
 
+    public void SkipSequence()
+    {
+        cancelRequested = true;
+        hasBeenSkipped = true;
+
+        if (currentSequence != null)
+        {
+            StopCoroutine(currentSequence);
+            currentSequence = null;
+        }
+
+        HideWeapon();
+    }
 
     public void HideWeapon()
     {
-        if(currentWeapon != null)
+        if (currentWeapon != null)
         {
             Destroy(currentWeapon);
+            currentWeapon = null;
         }
     }
 }
